c0b2757fab3c2fc5248ecaa4d2f65a5d9f9ea905
dextprojects <32615294+dextprojects@users.noreply.github.com>
2019-08-26 02:36:46 -0500

    Update GetUpdates.java

--- src/main/java/com/dext/rest/service/GetUpdates.java
+++ src/main/java/com/dext/rest/service/GetUpdates.java
@@ -1,39 +1,47 @@
 package com.dext.rest.service;
 
+import java.util.Arrays;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.dext.rest.entity.Articles;
 import com.dext.rest.entity.NewsEntity;
+import com.dext.rest.service.repository.NewsRepository;
 
 @Service
 public class GetUpdates {
-	
+	@Autowired
+	NewsRepository newsRepository;
+
 	@Scheduled(cron = "*/10 * * * * *")
 	public void getNewsUpdates() {
-		System.out.println("::Cron::");
+		System.out.println("::Getting Data from Google News for BBC News::");
 		Client client = ClientBuilder.newClient();
 
-		WebTarget webTarget = client
-				.target("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=19c8a10fe7d24f3fb3a7928f1132e84e");
+		WebTarget webTarget = client.target(
+				"https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=19c8a10fe7d24f3fb3a7928f1132e84e");
 
 		Invocation.Builder builder = webTarget.request(MediaType.APPLICATION_JSON);
 		Response response = builder.get();
-		System.out.println("response: " + response.getStatus());
-		System.out.println("response: " + response.getMediaType());
+		System.out.println("Response Status: " + response.getStatus());
 		NewsEntity newsEntities = response.readEntity(NewsEntity.class);
-		for(Articles article: newsEntities.getArticles()){
-			System.out.println("\nArticle Title: "+article.getTitle());
-			System.out.println("Article Description: "+article.getDescription()+"\n");
-		}
+		Arrays.stream(newsEntities.getArticles()).forEach(article -> {
+			if (!newsRepository.findByTitle(article.getTitle()).isPresent())
+				newsRepository.save(article);
+		});
+		System.out.println("::Data Stored in DB::");
+		System.out.println("Total Document stored in DB: " + newsRepository.count());
 	}
 
 }
